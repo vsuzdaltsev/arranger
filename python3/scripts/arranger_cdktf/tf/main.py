a66959c6968074ef245e3bbd395dec2262583a9b
vsuzdaltsev <vsevolod.suzdaltsev@gmail.com>
2024-11-11 17:58:46 +0300

    feat: invoke tasks for Terraform actions

--- python3/scripts/arranger_cdktf/tf/main.py
+++ python3/scripts/arranger_cdktf/tf/main.py
@@ -10,7 +10,7 @@ from arranger_cdktf.tf.arranger_terraform_stacks import *
 from arranger_conf.arranger_cdktf_conf import TfConf
 
 
-CLUSTER_NAME_ALIAS = sys.argv[1]
+TENANT = sys.argv[1]
 STACK = sys.argv[2]
 PROJECT_NAME = "tf"
 
@@ -19,29 +19,27 @@ if __name__ == "__main__":
     app = App(stack_traces=True)
     log = Log().logger(desc=str(app.__class__.__qualname__))
 
-    env_config = getattr(TfConf, CLUSTER_NAME_ALIAS.capitalize())
+    env_config = getattr(TfConf, TENANT.capitalize())
 
     def _class() -> Type:
         if STACK not in env_config.ALL_STACKS:
             raise ValueError(
-                f"Stacks suitable for '{CLUSTER_NAME_ALIAS}' cluster name alias "
-                f"are listed within the '{CLUSTER_NAME_ALIAS}' config. "
+                f"Stacks suitable for '{TENANT}' cluster name alias "
+                f"are listed within the '{TENANT}' config. "
                 f"Please ensure {env_config.__qualname__}.ALL_STACKS value contains '{STACK}'."
             )
 
         default_class_name = env_config.VALID_STACKS[PROJECT_NAME][STACK]["class_name"]
 
         try:
-            custom_class_name = (
-                f"{env_config.CLUSTER_NAME_ALIAS.capitalize()}{default_class_name}"
-            )
+            custom_class_name = f"{env_config.TENANT.capitalize()}{default_class_name}"
             log.warning(">> Trying to use class: %s.", custom_class_name)
 
             return globals()[custom_class_name]
         except KeyError:
             log.warning(
                 ">> Class %s%s not found, falling back to %s.",
-                env_config.CLUSTER_NAME_ALIAS.capitalize(),
+                env_config.TENANT.capitalize(),
                 default_class_name,
                 default_class_name,
             )
