8718b054bca09493093f6d1d801a993b097c34b8
vsuzdaltsev <vsevolod.suzdaltsev@gmail.com>
2024-11-15 11:46:02 +0300

    feat: spec arranger_conf

--- python3/packages/arranger_conf/arranger_conf/tests/test_arranger_cdktf_conf.py
+++ python3/packages/arranger_conf/arranger_conf/tests/test_arranger_cdktf_conf.py
@@ -2,7 +2,7 @@ from arranger_conf import ArrangerConf
 from arranger_conf.arranger_cdktf_conf import TfConf
 
 
-def tf_conf_tenant_class_name(tenant: str) -> str:
+def _class_name(tenant: str) -> str:
     return tenant.capitalize()
 
 
@@ -16,23 +16,17 @@ class TestTFConf:
         self, tenants_from_tf_conf
     ):
         for tenant in [
-            tf_conf_tenant_class_name(tenant=_t)
-            for _t in tenants_from_tf_conf
-            if _t != "local"
+            _class_name(tenant=_t) for _t in tenants_from_tf_conf if _t != "local"
         ]:
             assert isinstance(getattr(TfConf, tenant).ALL_STACKS, list)
             assert len(getattr(TfConf, tenant).ALL_STACKS) > 0
 
     def test_AWS_GLOBAL_REGION(self, tenants_from_tf_conf):
         assert [
-            getattr(TfConf, tf_conf_tenant_class_name(tenant=tenant)).AWS_GLOBAL_REGION
-            == "us-east-1"
+            getattr(TfConf, _class_name(tenant=tenant)).AWS_GLOBAL_REGION == "us-east-1"
             for tenant in tenants_from_tf_conf
         ]
 
     def test_TENANT(self, tenants_from_tf_conf):
         for tenant in tenants_from_tf_conf:
-            assert (
-                getattr(TfConf, tf_conf_tenant_class_name(tenant=tenant)).TENANT
-                == tenant
-            )
+            assert getattr(TfConf, _class_name(tenant=tenant)).TENANT == tenant
