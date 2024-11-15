7e87a2ebff05237007da8f1e1557ce5f8b8ded40
vsuzdaltsev <vsevolod.suzdaltsev@gmail.com>
2024-11-15 21:04:00 +0300

    feat: create EKS

--- python3/packages/arranger_conf/arranger_conf/arranger_cdktf_conf.py
+++ python3/packages/arranger_conf/arranger_conf/arranger_cdktf_conf.py
@@ -4,6 +4,19 @@ from .basic_conf import BasicConf
 class TfConf:
     class Development1(BasicConf):
         ALL_STACKS = ["vpc-stack", "eks-stack"]
+        EKS_NODE_GROUPS_CONFIG = [
+            {
+                "node_group_name": "node-group-1",
+                "capacity_type": "SPOT",  # TODO: select ON_DEMAND / SPOT ?
+                "instance_types": ["t3.xlarge"],
+                "autoscale_config": {
+                    "min_size": 1,
+                    "max_size": 3,
+                    "desired_size": 2,
+                },
+                "disk_size": 50,
+            },
+        ]
 
     class Local(BasicConf):
         ALL_STACKS = []
