699ed7ee0d200cb5715d9d4a59411cd366bd8be1
vsuzdaltsev <vsevolod.suzdaltsev@gmail.com>
2024-11-15 13:27:16 +0300

    feat: spec

--- python3/packages/arranger_globals/arranger_globals/tests/conftest.py
+++ python3/packages/arranger_globals/arranger_globals/tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
+from arranger_globals import CdktfGlobals
 
 @pytest.fixture(scope="function")
 def cdktf_globals_for_local_tenant():
-    from arranger_globals import CdktfGlobals
 
     return CdktfGlobals(tenant="local")
