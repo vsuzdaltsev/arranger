9e6475164ae0a0b100ad4d0cc0d9f7b3257df47d
vsuzdaltsev <vsevolod.suzdaltsev@gmail.com>
2024-12-02 13:47:20 +0300

    fix: cleanup

--- python3/packages/arranger_automation_aws/arranger_automation_aws/helpers/db_helper.py
+++ python3/packages/arranger_automation_aws/arranger_automation_aws/helpers/db_helper.py
@@ -5,7 +5,7 @@ from arranger_automation_aws.basic_resource import BasicAwsResource
 
 
 class DBHelper(BasicAwsResource):
-    def __init__(self, boto_client: type(AwsClient), cluster_name: str):
+    def __init__(self, boto_client: AwsClient, cluster_name: str):
         super().__init__(boto_client=boto_client)
         self._cluster_name = cluster_name
         self._endpoint = None
